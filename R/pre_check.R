2735a4dcfd44b809ff228fd3354379819d9a3f23
xiayh17 <xiayh17@gmail.com>
2021-10-10 17:30:14 +0800

    🏗pre_check have done

---
+++ R/pre_check.R
@@ -0,0 +1,167 @@
+#' Pre-check you data by PCA and Heat maps
+#'
+#' A previous check function for overview the data sets.
+#'
+#' @param counts_data a counts data frame of rows in genes and columns in samples
+#' @param group_list a list ordered by samples in counts_data
+#' @param dir a directory to store results
+#'
+#' @importFrom glue glue
+#' @importFrom edgeR cpm
+#' @importFrom fs dir_exists dir_create
+#'
+#' @return a directory contains figures for data QC check
+#' @export
+#'
+#' @examples
+#' pre_check(counts_input, group_list)
+pre_check <- function(counts_data, group_list, dir = ".") {
+  if (!fs::dir_exists(dir)) {
+    fs::dir_create(dir)
+  }
+  exprSet=counts_data
+  dat=log2(cpm(exprSet)+1)
+  pca_check(dat,group_list,dir = dir)
+  message(glue("🌲 PCA checking have done, a plot was store in {dir}."))
+  corall_check(dat,group_list,dir = dir)
+  message(glue("🌲 Correlation checking have done, a plot was store in {dir}."))
+  cor500_check(exprSet,group_list,dir = dir)
+  message(glue("🌲 Correlation to top 500 genes checking have done, a plot was store in {dir}."))
+  top1000_check(dat,group_list,dir = dir)
+  message(glue("🌲 Standard Deviation top 1000 genes checking have done, a plot was store in {dir}."))
+}
+
+#' PCA QC check
+#'
+#' check data quality by PCA plot
+#'
+#' @param data a cpm data frame of rows in genes and columns in samples
+#' @param list a list ordered by samples in data
+#' @param dir a directory to store results
+#'
+#' @importFrom FactoMineR PCA
+#' @importFrom factoextra fviz_pca_ind
+#' @importFrom ggplot2 theme element_text labs ggsave
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom glue glue
+#'
+#' @return a figure of PCA
+#'
+#' @examples
+#' pca_check(data, list)
+#' @noRd
+pca_check <- function(data, list, dir = ".") {
+  filename = glue('{dir}/all_samples_PCA_by_type.pdf')
+  dat=t(data)
+  dat.pca <- PCA(dat , graph = FALSE)#现在dat最后一列是group_list，需要重新赋值给一个dat.pca,这个矩阵是不含有分组信息的
+  p <- fviz_pca_ind(dat.pca,
+               geom.ind = "point", # show points only (nbut not "text")
+               col.ind =  list, # color by groups
+               palette = RColorBrewer::brewer.pal(3,"Set2")[1:2],
+               addEllipses = TRUE, # Concentration ellipses
+               legend.title = "Groups"
+  ) + theme(plot.title = element_text(face = "bold")) +
+    labs(title = "all samples - PCA")
+  ggsave(filename,p, width = 400/100, height = 350/100, dpi = 300, units = "in", limitsize = FALSE)
+}
+
+#' Correlation of all samples and all genes
+#'
+#' Check data quality by calculating correlation of all samples
+#'
+#' @param data a cpm data frame of rows in genes and columns in samples
+#' @param list a list ordered by samples in data
+#' @param dir a directory to store results
+#'
+#' @importFrom glue glue
+#' @importFrom pheatmap pheatmap
+#'
+#' @return a Heatmap shows correlation of all samples
+#'
+#' @examples
+#' corall_check(data, list)
+#' @noRd
+corall_check <- function(data, list, dir = ".") {
+  filename = glue('{dir}/cor_all.pdf')
+  colD=data.frame(group=list)
+  rownames(colD)=colnames(data)
+  m=cor(data)
+  pheatmap( m ,
+                      annotation_col = colD,
+                      show_rownames = F,
+                      width = 400/100,
+                      height = 350/100,
+                      main = "Correlation by all genes",
+                      filename = filename)
+  # ggsave(filename, width = 400/100, height = 350/100, dpi = 300, units = "in", limitsize = FALSE)
+}
+
+#' Correlation of all samples and top 500 genes
+#'
+#' Check data quality by calculating correlation of all samples but top 500 genes
+#'
+#' @param counts_data a counts data frame of rows in genes and columns in samples
+#' @param list a list ordered by samples in data
+#' @param dir a directory to store results
+#'
+#' @importFrom glue glue
+#' @importFrom pheatmap pheatmap
+#'
+#' @return a Heatmap shows correlation of all samples to 500 genes
+#'
+#' @examples
+#' cor500_check(counts_input, list)
+#' @noRd
+cor500_check <- function(data, list, dir = ".") {
+  filename = glue('{dir}/cor_top500.pdf')
+  exprSet=data[apply(data,1, function(x) sum(x>1) > 5),]
+  exprSet=log(edgeR::cpm(exprSet)+1)
+  exprSet=exprSet[names(sort(apply(exprSet, 1,mad),decreasing = T)[1:500]),]
+  colD=data.frame(group=list)
+  rownames(colD)=colnames(exprSet)
+  M=cor(exprSet)
+  pheatmap::pheatmap(M,
+                     show_rownames = F,
+                     annotation_col = colD,
+                     width = 400/100,
+                     height = 350/100,
+                     main = "Correlation by top 500",
+                     filename = filename)
+  # ggsave(filename, width = 400/100, height = 350/100, dpi = 300, units = "in", limitsize = FALSE)
+}
+
+#' Heatmap of all samples and Top1000 genes
+#'
+#' Check data quality by plotting heatmap of top 1000 genes
+#'
+#' @param data a cpm data frame of rows in genes and columns in samples
+#' @param list a list ordered by samples in data
+#' @param dir a directory to store results
+#'
+#' @importFrom glue glue
+#' @importFrom pheatmap pheatmap
+#'
+#' @return a Heatmap shows top100 genes of all samples
+#'
+#' @examples
+#' top1000_check(data, list)
+#' @noRd
+top1000_check <- function(data, list, dir = ".") {
+  filename = glue('{dir}/heatmap_top1000_sd.pdf')
+  dat = data
+  cg=names(tail(sort(apply(dat,1,sd)),1000))
+  n=t(scale(t(dat[cg,]))) # 'scale'可以对log-ratio数值进行归一化
+  n[n>2]=2
+  n[n< -2]= -2
+  n[1:4,1:4]
+  ac=data.frame(group=group_list)
+  rownames(ac)=colnames(n)
+  pheatmap::pheatmap( n ,
+                      annotation_col = ac,
+                      show_rownames = F,
+                      width = 400/100,
+                      height = 350/100,
+                      main = "SD Top 1000 genes",
+                      filename = filename)
+  # ggsave(filename, width = 400/100, height = 350/100, dpi = 300, units = "in", limitsize = FALSE)
+}
