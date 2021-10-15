b63653e3bb17360e2a3d7e30e6db35efb4c8095e
xiayh17 <xiayh17@gmail.com>
2021-10-15 22:08:50 +0800

    :rocket: update ci script

--- dev/ci.R
+++ dev/ci.R
@@ -1,7 +1,7 @@
 # usethis::use_github_action_check_standard()
 # usethis::use_github_actions_badge()
-usethis::use_testthat()
-usethis::use_coverage()
+# usethis::use_testthat()
+# usethis::use_coverage()
 library(covr); codecov()
 
 # usethis::use_github_action("test-coverage")
