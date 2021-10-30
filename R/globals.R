34bbb429606b807c1ab68ab3295e4c579c890d9d
xiayh17 <xiayh17@gmail.com>
2021-10-30 17:04:04 +0800

    :rocket: for check

--- R/globals.R
+++ R/globals.R
@@ -1,2 +1,3 @@
 utils::globalVariables(c("log2FoldChange", "pvalue", "group", "rn",
-                         "Count", "myY", "ONTOLOGY", "Description", ""))
+                         "Count", "myY", "ONTOLOGY", "Description", "fulog10pvalue"
+                         ,"upordown"))
