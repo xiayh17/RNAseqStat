92b698b1c8ff649cf98960ca9fa99a3df7e6059a
xiayh17 <xiayh17@gmail.com>
2021-11-01 11:10:27 +0800

    :sparkles: start all run function

---
+++ R/runAll.R
@@ -0,0 +1,13 @@
+runAll <- function(count_data, group_list) {
+
+  message(glue("Step1: Check you data"))
+
+  message(glue("Step2: DEG analysis"))
+
+  message(glue("Step3: EnrichGO analysis"))
+
+  message(glue("Step4: EnrichKEGG analysis"))
+
+  message(glue("Step5: EnrichgseKEGG analysis"))
+
+}
